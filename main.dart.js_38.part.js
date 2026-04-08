8dc36cd0b79cd927fd4d8d57c314ab77cef56080
Ruslan.D <x.stelvia@gmail.com>
2026-04-08 23:32:38 +0600

    f

---
+++ main.dart.js_38.part.js
@@ -0,0 +1,5 @@
+((a,b)=>{a[b]=a[b]||{}})(self,"$__dart_deferred_initializers__")
+$__dart_deferred_initializers__.current=function(a,b,c,$){var A
+A=c[0]
+var z=a.updateTypes([]);(function staticFields(){$.c52=A.b([["West Island",-12.15681,96.82251,"Cocos (Keeling) Islands","Indian/Cocos"],["Flying Fish Cove",-10.42172,105.67912,"Christmas Island","Indian/Christmas"]],A.a3("x<J<@>>"))})()};
+(a=>{a["ayIi6bTCWU2life0/1J1nZdQFiw="]=a.current})($__dart_deferred_initializers__);
---
+++ main.dart.js_39.part.js
@@ -0,0 +1,5 @@
+((a,b)=>{a[b]=a[b]||{}})(self,"$__dart_deferred_initializers__")
+$__dart_deferred_initializers__.current=function(a,b,c,$){var A
+A=c[0]
+var z=a.updateTypes([]);(function staticFields(){$.c53=A.b([["Grytviken",-54.28111,-36.5092,"South Georgia & South Sandwich Islands","Atlantic/South_Georgia"],["Port-aux-Fran\xe7ais",-49.34916,70.21937,"French Southern Territories","Indian/Kerguelen"]],A.a3("x<J<@>>"))})()};
+(a=>{a["HsfjbTEkVUjmSS0vhKWhtd1jHGM="]=a.current})($__dart_deferred_initializers__);
